ecd9bf640e7a1c902b945a01292e7c283958ed67
Watson-Sei <seinabehack@gmail.com>
2021-09-27 13:23:14 +0900

    create metamask send transfer eth

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,99 @@
 import React from 'react';
-import logo from './logo.svg';
+import detectEthereumProvider from '@metamask/detect-provider';
+import Web3 from 'web3';
 import './App.css';
 
 function App() {
+
+  const [address, setAddress] = React.useState<string>("");
+  const [metaState, setMetastate] = React.useState<boolean>(false);
+  const [price, setPrice] = React.useState<string>("");
+
+  var web3: Web3;
+
+  const enable = async () => {
+    const provider = await detectEthereumProvider();
+    if (provider) {
+      console.log('Welcome to MetaMask User!');
+        setMetastate(true);
+        web3 = new Web3(Web3.givenProvider)
+        web3.eth.defaultChain = "ropsten";
+    } else {
+      console.log('Please Install MetaMask!');
+      setMetastate(false);
+    }
+  }
+
+  enable();
+
+  const connect = async () => {
+    const accounts = await web3.eth.requestAccounts();
+    setAddress(accounts[0]);
+  }
+
+  const handleChange = (event: { target: { name: any; value: React.SetStateAction<string>; }; }) => {
+    switch (event.target.name) {
+      case 'price': 
+        setPrice(event.target.value);
+        break
+      default:
+        console.log('key not found');
+    }
+  }
+
+  const sendEthereum = (event: { preventDefault: () => void; }) => {
+    event.preventDefault();
+    console.log("to:", web3.utils.isAddress(address))
+    console.log("from", web3.utils.isAddress("0xB863d22442084d3B3a0D6321a0F2b61852215FDD"))
+    web3.eth.sendTransaction({
+      from: address,
+      to: '0x...', // 送り主のアドレスを指定してください
+      value: web3.utils.toWei(price, 'ether')
+    })
+      .on('receipt', function(receipt) {
+        console.log(receipt);
+      })
+      .on('error', console.error);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <div className="container">
+      {metaState ? (
+        <>
+        <img src={`${process.env.PUBLIC_URL}/metamask.gif`} alt="" />
+          <h2>Welcome to MetaMask User!</h2>
+          <p>{address}</p>
+          {!address ? (
+            <button onClick={connect}>Connect</button>
+          ): (
+            <>
+              <div>
+                <form onSubmit={sendEthereum}>
+                  <label>
+                    Price(eth): 
+                    <input 
+                      type="text" 
+                      name="price" 
+                      value={price} 
+                      onChange={handleChange} 
+                      onKeyPress={e => {
+                        if (e.key === 'Enter') e.preventDefault();
+                      }} 
+                    />
+                  </label>
+                  <button type="submit">送金</button>
+                </form>
+              </div>
+              <p>disconnectしたい方は<a href="https://metamask.zendesk.com/hc/en-us/articles/360059535551-Disconnect-wallet-from-Dapp">こちらから</a></p>
+            </>
+          )}
+        </>
+      ): (
+        <h2>Please Install MetaMask!</h2>
+      )}
+      </div>
+    </React.Fragment>
   );
 }
 
