e170fc38ff68c972158be7a9b54e4ddc2123f983
Watson-Sei <seinabehack@gmail.com>
2021-10-09 00:12:05 +0900

    fix state

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect,useState,useRef} from 'react';
 import detectEthereumProvider from '@metamask/detect-provider';
 import Web3 from 'web3';
 import './App.css';
@@ -232,65 +232,69 @@ const ERC20TransferABI: any = [
 
 function App() {
 
-  const [address, setAddress] = React.useState<string>(""); // ユーザーのウォレットアドレス
-  const [balance, setBalance] = React.useState<string | null>(null); // ユーザーのJPYC残高を格納
-  const [transfer, setTransfer] = React.useState<boolean>(false); // 送金中はTrueでボタンを無効化
+  // useRefで値を保持します
+  const web3 = useRef<Web3 | null>(null);
+  const address = useRef<string>("");
+  const jpyc = useRef<any | null>(null);
+  // 長期間の保持が必要じゃない場合useState
+  const [balance, setBalance] = useState<string>(""); 
+  const [transfer, setTransfer] = useState<boolean>(false);
 
-  var web3: Web3;
+  // DOM読み込み時一度だけと、balanceの値が変化した際に呼び出します
+  useEffect(() => {
+    const f = async () => {
+        const provider = await detectEthereumProvider();
+        if (provider && window.ethereum?.isMetaMask) {
+            console.log('Welcome to MetaMask User!');
+            // web3のdefault設定(自由)
+            web3.current = new Web3(Web3.givenProvider);
+            web3.current.eth.defaultChain = "ropsten";
 
-  const enable = async () => {
-    const provider = await detectEthereumProvider();
-    if (provider && window.ethereum?.isMetaMask) {
-      console.log('Welcome to MetaMask User!');
-        web3 = new Web3(Web3.givenProvider)
-        web3.eth.defaultChain = "ropsten";
-
-        const accounts = await web3.eth.requestAccounts();
-        setAddress(accounts[0]);
-        CheckBalance();
-    } else {
-      console.log('Please Install MetaMask!');
-      window.location.href = "https://chrome.google.com/webstore/detail/metamask/nkbihfbeogaeaoehlefnkodbefgpgknn?hl=ja";
+            // ユーザーのアカウントを保存します(後でuseStateか確認する)
+            const accounts = await web3.current.eth.requestAccounts();
+            address.current = accounts[0];
+            // Contractの値を永久保持させる
+            jpyc.current = new web3.current.eth.Contract(ERC20TransferABI, tokenAddress);
+            CheckBalance();
+        } else {
+            console.log('Please Install MetaMask!');
+        }
     }
-  }
+    f();
+  }, [balance])
 
-  enable();
-
-  // 残高確認ボタンや残高情報更新の際に発火させる
+  // ユーザーの現在のJPYCの残高取得関数
   const CheckBalance = async () => {
-    if (address) {
-      const contract = new web3.eth.Contract(ERC20TransferABI, tokenAddress);
-      const decimals = await contract.methods.decimals().call();
-      const BNbalance = ((new BigNumber(await contract.methods.balanceOf(address).call())).div(10**decimals)).toString();
-      setBalance(BNbalance);
-    }
+      if (address.current !== "") {
+        const decimals = await jpyc.current.methods.decimals().call();
+        const BNbalance = ((new BigNumber(await jpyc.current.methods.balanceOf(address.current).call())).div(10**decimals)).toString();
+        setBalance(BNbalance);
+      }
   }
 
-  // toAddressに100JPYCを送金します
+  // 100JPYCを投げ銭する関数
   const OneCoinCheering = async () => {
-    if (address && Number(balance) >= 100 && toAddress !== address) {
-      const contract = new web3.eth.Contract(ERC20TransferABI, tokenAddress);
-      
-      const jpycDecimals = web3.utils.toBN(18);
-      const jpycAmountToTransferFrom = web3.utils.toBN(100);
-      const caluclatedTransferValue = web3.utils.toHex(jpycAmountToTransferFrom.mul(web3.utils.toBN(10).pow(jpycDecimals)));
+      console.log(balance)
+      if (address.current !== '' && Number(balance) >= 100 && address.current !== toAddress) {
+        const jpycDecimals: any = web3.current?.utils.toBN(18);
+        const jpycAmountToTransferFrom: any = web3.current?.utils.toBN(100);
+        const caluclatedTransferValue = web3.current?.utils.toHex(jpycAmountToTransferFrom?.mul(web3.current.utils.toBN(10).pow(jpycDecimals)));
 
-      contract.methods.transfer(toAddress, caluclatedTransferValue)
-        .send({from: address}, () => {
-        })
-        .on('transactionHash', function(hash: any) {
-          setTransfer(true);
-          console.log(hash);
-        })
-        .on('receipt', function(receipt: any) {
-          setTransfer(false);
-          CheckBalance();
-        })
-        .on('error', function(error: any) {
-          setTransfer(false);
-          CheckBalance();
-        });
-    }
+        jpyc.current.methods.transfer(toAddress, caluclatedTransferValue)
+            .send({from: address.current})
+            .on('transactionHash', function(hash: any) {
+                setTransfer(true);
+                console.log(hash);
+            })
+            .on('receipt', function(receipt: any) {
+                setTransfer(false);
+                CheckBalance();
+            })
+            .on('error', function(error: any) {
+                setTransfer(false);
+                CheckBalance();
+            });
+      }
   }
 
   return (
@@ -298,7 +302,7 @@ function App() {
       {address && (
         <div style={{ width: '250px', height: 'auto', border: '3px solid #16449A', textAlign: 'center', borderRadius: '20px', padding: '10px'}}>
           <p style={{ fontWeight: 'bold' }}>JPYCを投げ銭する</p>
-          <Button disabled={transfer} variant="contained" onClick={OneCoinCheering}>100JPYC送金</Button>
+          <Button disabled={transfer} variant="contained" onClick={OneCoinCheering} >100JPYC送金</Button>
         </div>
       )}
     </React.Fragment>
