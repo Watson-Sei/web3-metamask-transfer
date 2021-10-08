a50d204c48d923ca0b8dd3a54c293ec6ed5a5ef8
Watson-Sei <seinabehack@gmail.com>
2021-10-08 16:16:30 +0900

    add jpyc cheering

--- src/App.tsx
+++ src/App.tsx
@@ -2,95 +2,305 @@ import React from 'react';
 import detectEthereumProvider from '@metamask/detect-provider';
 import Web3 from 'web3';
 import './App.css';
+import BigNumber from "bignumber.js";
+import Button from '@mui/material/Button';
+
+const toAddress: string = "0xB863d22442084d3B3a0D6321a0F2b61852215FDD";
+const tokenAddress: string = "0xbD9c419003A36F187DAf1273FCe184e1341362C0";
+const ERC20TransferABI: any = [
+  {
+      "constant": true,
+      "inputs": [],
+      "name": "name",
+      "outputs": [
+          {
+              "name": "",
+              "type": "string"
+          }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+  },
+  {
+      "constant": false,
+      "inputs": [
+          {
+              "name": "_spender",
+              "type": "address"
+          },
+          {
+              "name": "_value",
+              "type": "uint256"
+          }
+      ],
+      "name": "approve",
+      "outputs": [
+          {
+              "name": "",
+              "type": "bool"
+          }
+      ],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function"
+  },
+  {
+      "constant": true,
+      "inputs": [],
+      "name": "totalSupply",
+      "outputs": [
+          {
+              "name": "",
+              "type": "uint256"
+          }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+  },
+  {
+      "constant": false,
+      "inputs": [
+          {
+              "name": "_from",
+              "type": "address"
+          },
+          {
+              "name": "_to",
+              "type": "address"
+          },
+          {
+              "name": "_value",
+              "type": "uint256"
+          }
+      ],
+      "name": "transferFrom",
+      "outputs": [
+          {
+              "name": "",
+              "type": "bool"
+          }
+      ],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function"
+  },
+  {
+      "constant": true,
+      "inputs": [],
+      "name": "decimals",
+      "outputs": [
+          {
+              "name": "",
+              "type": "uint8"
+          }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+  },
+  {
+      "constant": true,
+      "inputs": [
+          {
+              "name": "_owner",
+              "type": "address"
+          }
+      ],
+      "name": "balanceOf",
+      "outputs": [
+          {
+              "name": "balance",
+              "type": "uint256"
+          }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+  },
+  {
+      "constant": true,
+      "inputs": [],
+      "name": "symbol",
+      "outputs": [
+          {
+              "name": "",
+              "type": "string"
+          }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+  },
+  {
+      "constant": false,
+      "inputs": [
+          {
+              "name": "_to",
+              "type": "address"
+          },
+          {
+              "name": "_value",
+              "type": "uint256"
+          }
+      ],
+      "name": "transfer",
+      "outputs": [
+          {
+              "name": "",
+              "type": "bool"
+          }
+      ],
+      "payable": false,
+      "stateMutability": "nonpayable",
+      "type": "function"
+  },
+  {
+      "constant": true,
+      "inputs": [
+          {
+              "name": "_owner",
+              "type": "address"
+          },
+          {
+              "name": "_spender",
+              "type": "address"
+          }
+      ],
+      "name": "allowance",
+      "outputs": [
+          {
+              "name": "",
+              "type": "uint256"
+          }
+      ],
+      "payable": false,
+      "stateMutability": "view",
+      "type": "function"
+  },
+  {
+      "payable": true,
+      "stateMutability": "payable",
+      "type": "fallback"
+  },
+  {
+      "anonymous": false,
+      "inputs": [
+          {
+              "indexed": true,
+              "name": "owner",
+              "type": "address"
+          },
+          {
+              "indexed": true,
+              "name": "spender",
+              "type": "address"
+          },
+          {
+              "indexed": false,
+              "name": "value",
+              "type": "uint256"
+          }
+      ],
+      "name": "Approval",
+      "type": "event"
+  },
+  {
+      "anonymous": false,
+      "inputs": [
+          {
+              "indexed": true,
+              "name": "from",
+              "type": "address"
+          },
+          {
+              "indexed": true,
+              "name": "to",
+              "type": "address"
+          },
+          {
+              "indexed": false,
+              "name": "value",
+              "type": "uint256"
+          }
+      ],
+      "name": "Transfer",
+      "type": "event"
+  }
+];
 
 function App() {
 
   const [address, setAddress] = React.useState<string>("");
-  const [metaState, setMetastate] = React.useState<boolean>(false);
-  const [price, setPrice] = React.useState<string>("");
+  const [balance, setBalance] = React.useState<string | null>(null);
+  const [transfer, setTransfer] = React.useState<boolean>(false);
 
   var web3: Web3;
 
   const enable = async () => {
     const provider = await detectEthereumProvider();
-    if (provider) {
+    if (provider && window.ethereum?.isMetaMask) {
       console.log('Welcome to MetaMask User!');
-        setMetastate(true);
         web3 = new Web3(Web3.givenProvider)
         web3.eth.defaultChain = "ropsten";
+
+        const accounts = await web3.eth.requestAccounts();
+        setAddress(accounts[0]);
+        CheckBalance();
     } else {
       console.log('Please Install MetaMask!');
-      setMetastate(false);
+      window.location.href = "https://chrome.google.com/webstore/detail/metamask/nkbihfbeogaeaoehlefnkodbefgpgknn?hl=ja";
     }
   }
 
   enable();
 
-  const connect = async () => {
-    const accounts = await web3.eth.requestAccounts();
-    setAddress(accounts[0]);
-  }
-
-  const handleChange = (event: { target: { name: any; value: React.SetStateAction<string>; }; }) => {
-    switch (event.target.name) {
-      case 'price': 
-        setPrice(event.target.value);
-        break
-      default:
-        console.log('key not found');
+  // 残高確認ボタンや残高情報更新の際に発火させる
+  const CheckBalance = async () => {
+    if (address) {
+      const contract = new web3.eth.Contract(ERC20TransferABI, tokenAddress);
+      const decimals = await contract.methods.decimals().call();
+      const BNbalance = ((new BigNumber(await contract.methods.balanceOf(address).call())).div(10**decimals)).toString();
+      setBalance(BNbalance);
     }
   }
 
-  const sendEthereum = (event: { preventDefault: () => void; }) => {
-    event.preventDefault();
-    web3.eth.sendTransaction({
-      from: address,
-      to: '0x...', // 送り主のアドレスを指定してください
-      value: web3.utils.toWei(price, 'ether')
-    })
-      .on('receipt', function(receipt) {
-        console.log(receipt);
-      })
-      .on('error', console.error);
+  // toAddressに100JPYCを送金します
+  const OneCoinCheering = async () => {
+    if (address && Number(balance) >= 100 && toAddress !== address) {
+      const contract = new web3.eth.Contract(ERC20TransferABI, tokenAddress);
+      
+      const jpycDecimals = web3.utils.toBN(18);
+      const jpycAmountToTransferFrom = web3.utils.toBN(100);
+      const caluclatedTransferValue = web3.utils.toHex(jpycAmountToTransferFrom.mul(web3.utils.toBN(10).pow(jpycDecimals)));
+
+      contract.methods.transfer(toAddress, caluclatedTransferValue)
+        .send({from: address}, () => {
+        })
+        .on('transactionHash', function(hash: any) {
+          setTransfer(true);
+          console.log(hash);
+        })
+        .on('receipt', function(receipt: any) {
+          setTransfer(false);
+          CheckBalance();
+        })
+        .on('error', function(error: any) {
+          setTransfer(false);
+          CheckBalance();
+        });
+    }
   }
 
   return (
     <React.Fragment>
-      <div className="container">
-      {metaState ? (
-        <>
-        <img src={`${process.env.PUBLIC_URL}/metamask.gif`} alt="" />
-          <h2>Welcome to MetaMask User!</h2>
-          <p>{address}</p>
-          {!address ? (
-            <button onClick={connect}>Connect</button>
-          ): (
-            <>
-              <div>
-                <form onSubmit={sendEthereum}>
-                  <label>
-                    Price(eth): 
-                    <input 
-                      type="text" 
-                      name="price" 
-                      value={price} 
-                      onChange={handleChange} 
-                      onKeyPress={e => {
-                        if (e.key === 'Enter') e.preventDefault();
-                      }} 
-                    />
-                  </label>
-                  <button type="submit">送金</button>
-                </form>
-              </div>
-              <p>disconnectしたい方は<a href="https://metamask.zendesk.com/hc/en-us/articles/360059535551-Disconnect-wallet-from-Dapp">こちらから</a></p>
-            </>
-          )}
-        </>
-      ): (
-        <h2>Please Install MetaMask!</h2>
+      {address && (
+        <div style={{ width: '250px', height: 'auto', border: '3px solid #16449A', textAlign: 'center', borderRadius: '20px', padding: '10px'}}>
+          <p style={{ fontWeight: 'bold' }}>JPYCを投げ銭する</p>
+          <Button disabled={transfer} variant="contained" onClick={OneCoinCheering}>100JPYC送金</Button>
+        </div>
       )}
-      </div>
     </React.Fragment>
   );
 }
